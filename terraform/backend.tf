d6e847884f349b09e38b38be1592ee6eef9c668a
anotherbuginthecode <mangone.devops@gmail.com>
2022-07-08 14:09:56 +0200

    Add github actions to provisioning infra with terraform and deploy webapp into s3

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -2,7 +2,7 @@ terraform {
   backend "s3" {
     bucket     = "mangone-s3-bucket/terraform/"
     key        = "deploy-vuejs-app-to-s3-cloudfront/terraform.tfstate"
-    region     = "eu-west-1"
+    region     = var.region
     access_key = var.access_key
     secret_key = var.secret_key
   }
--- terraform/cloudfront.tf
+++ terraform/cloudfront.tf
@@ -95,3 +95,7 @@ resource "aws_cloudfront_distribution" "s3_distribution" {
 output "cloudfront_domain_name" {
   value = aws_cloudfront_distribution.s3_distribution.domain_name
 }
+
+output "cloudfront_distribution" {
+  value = aws_cloudfront_distribution.s3_distribution.id
+}
