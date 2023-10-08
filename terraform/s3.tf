f97a90734249711ae4070f02d3bd615c520b9fca
Alessandro Mangone <38384237+anotherbuginthecode@users.noreply.github.com>
2023-10-08 10:51:45 +0200

    Update s3.tf
    fixing error: AccessControlListNotSupported: The bucket does not allow ACLs

--- terraform/s3.tf
+++ terraform/s3.tf
@@ -88,6 +88,20 @@ resource "aws_s3_bucket_public_access_block" "website" {
   //restrict_public_buckets = true
 }
 
+resource "aws_s3_bucket_ownership_controls" "acl" {
+  bucket = aws_s3_bucket.website.id
+  rule {
+    object_ownership = "ObjectWriter"
+  }
+}
+
+resource "aws_s3_bucket_acl" "acl" {
+  depends_on = [aws_s3_bucket_ownership_controls.acl]
+
+  bucket = aws_s3_bucket.website.id
+  acl    = "private"
+}
+
 output "bucket_name" {
   value = aws_s3_bucket.website.id
 }
